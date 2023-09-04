ff934508db63b56bf2aa07ea057325017d9b4c1d
DaiND <duynguyen1241@gmail.com>
2023-09-04 21:57:00 +0700

    edit service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,14 +5,10 @@ use AwesomeJapanese\AwesomeJapanese;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    protected $defer = true;
-
     public function register()
     {
         $this->app->singleton(AwesomeJapanese::class, function () {
             return new AwesomeJapanese();
         });
-
-        $this->app->alias(AwesomeJapanese::class, 'AwesomeJapanese');
     }
 }
